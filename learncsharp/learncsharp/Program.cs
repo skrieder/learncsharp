139beaa90e1fe65e41a851acf8f05500b5875431
Scott J. Krieder <skrieder@iit.edu>
2015-01-25 18:58:39 -0600

    comment out for loop to print types

--- learncsharp/learncsharp/Program.cs
+++ learncsharp/learncsharp/Program.cs
@@ -23,7 +23,7 @@ namespace learncsharp
             ulong i8 = 18446744073709551615;
 
             // print examples
-            Console.WriteLine("i1 = {0}",i1);
+            Console.WriteLine("i1 = {0}", i1);
             Console.WriteLine("i2 = {0}", i2);
             Console.WriteLine("i3 = {0}", i3);
             Console.WriteLine("i4 = {0}", i4);
@@ -32,11 +32,26 @@ namespace learncsharp
             Console.WriteLine("i7 = {0}", i7);
             Console.WriteLine("i8 = {0}", i8);
 
-           // String s1 = (String)i1.GetType();
-
             // print types
             Console.WriteLine("i1 has a type of: {0}", i1.GetType());
+            Console.WriteLine("i2 has a type of: {0}", i2.GetType());
+            Console.WriteLine("i3 has a type of: {0}", i3.GetType());
+            Console.WriteLine("i4 has a type of: {0}", i4.GetType());
+            Console.WriteLine("i5 has a type of: {0}", i5.GetType());
+            Console.WriteLine("i6 has a type of: {0}", i6.GetType());
+            Console.WriteLine("i7 has a type of: {0}", i7.GetType());
+            Console.WriteLine("i8 has a type of: {0}", i8.GetType());
 
+            // for loop to print types
+            /*
+            for (int j = 1; j < 9; j++)
+            {
+                Console.WriteLine("i{0} has a type of: {1}", j, "i" + j.GetType());
+            }
+             * */
+
+
+            // readline to show console output
             Console.ReadLine();
 
         }
